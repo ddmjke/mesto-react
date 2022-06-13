c94aa8d09f9ee61390393a793456a1e293367101
Dmitry Konychev <dalexeych@gmail.com>
2022-06-13 22:57:04 +0300

    ref: profile context

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Footer from './Footer';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
 import React from 'react';
+import mestoApi from '../utils/Api';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,8 +16,23 @@ export default class App extends React.Component {
       isEditAvatarPopupOpen: false,
       isImagePopupOpen: false,
       selectedCard: null,
+      currentUser: null,
+      cards: null,
     }
   }
+
+  componentDidMount() {
+    Promise.all([mestoApi.getUser(), mestoApi.getCards()])
+      .then(([user, cards]) => {
+        this.setState({
+          currentUser: user,
+          cards: cards,
+        })
+        console.log(this.state.currentUser)
+      })
+      .catch(err => console.log(`Failed to load initial info : ${err}`));
+  }
+
 
   handleEditProfileClick = () => {
     this.setState({isEditProfilePopupOpen: true});
@@ -49,76 +66,79 @@ export default class App extends React.Component {
   render() {
   return (
     <>
-      <Header />
+      <CurrentUserContext.Provider value={this.state.currentUser || ''}>
 
-      <Main 
-        onEditProfile={this.handleEditProfileClick}
-        onAddPlace={this.handleAddPlaceClick}
-        onEditAvatar={this.handleEditAvatarClick}
-        onCardClick={this.handleCardClick}
-      />
-      
-      <Footer />
+        <Header />
 
-      <PopupWithForm
-        name="profile"
-        title="Редактировать профиль"
-        isOpen={this.state.isEditProfilePopupOpen}
-        onClose={this.closeAllPopups}
-        buttonText="Сохранить"
-        >
-        <form className="pop-up__form" name="user-info" noValidate>
-          <label className="pop-up__field">
-            <input className="pop-up__input pop-up__input_field_name" name="user-name" type="text" id="user-name" placeholder="Имя" required minLength="2" maxLength="40"/>
-            <span className="pop-up__input-error user-name-error">!!!</span>
-          </label>
-          <label className="pop-up__field">
-            <input className="pop-up__input pop-up__input_field_info" name="user-profession" type="text" id="user-profession" placeholder="Род деятельности" required minLength="2" maxLength="200"/>
-            <span className="pop-up__input-error user-profession-error">!!!</span>
-          </label>
-        </form>
-      </PopupWithForm>
+        <Main 
+          onEditProfile={this.handleEditProfileClick}
+          onAddPlace={this.handleAddPlaceClick}
+          onEditAvatar={this.handleEditAvatarClick}
+          onCardClick={this.handleCardClick}
+        />
+        
+        <Footer />
 
-      <PopupWithForm
-        name="avatar"
-        title="Обновить аватар"
-        isOpen={this.state.isEditAvatarPopupOpen}
-        onClose={this.closeAllPopups}
-        buttonText="Сохранить"
-        >
-        <form className="pop-up__form" name="user-pic" noValidate>
-          <label className="pop-up__field"> 
-            <input className="pop-up__input pop-up__input_field_avatar-link" type="url" id="avatar" placeholder="Ссылка на аватар" required/>
-            <span className="pop-up__input-error avatar-error">!!!</span>          
-          </label>
-        </form>
-      </PopupWithForm>
+        <PopupWithForm
+          name="profile"
+          title="Редактировать профиль"
+          isOpen={this.state.isEditProfilePopupOpen}
+          onClose={this.closeAllPopups}
+          buttonText="Сохранить"
+          >
+          <form className="pop-up__form" name="user-info" noValidate>
+            <label className="pop-up__field">
+              <input className="pop-up__input pop-up__input_field_name" name="user-name" type="text" id="user-name" placeholder="Имя" required minLength="2" maxLength="40"/>
+              <span className="pop-up__input-error user-name-error">!!!</span>
+            </label>
+            <label className="pop-up__field">
+              <input className="pop-up__input pop-up__input_field_info" name="user-profession" type="text" id="user-profession" placeholder="Род деятельности" required minLength="2" maxLength="200"/>
+              <span className="pop-up__input-error user-profession-error">!!!</span>
+            </label>
+          </form>
+        </PopupWithForm>
 
-      <PopupWithForm 
-        name="place"
-        title="Новое место"
-        isOpen={this.state.isAddPlacePopupOpen}
-        onClose={this.closeAllPopups}
-        buttonText="Сохранить"
-        >
-        <form className="pop-up__form" name="place-form" noValidate>
-          <label className="pop-up__field">
-            <input className="pop-up__input pop-up__input_field_place-name" type="text" id="name" placeholder="Название" required minLength="2" maxLength="30"/>
-            <span className="pop-up__input-error name-error">!!!</span>          
-          </label>
-          <label className="pop-up__field"> 
-            <input className="pop-up__input pop-up__input_field_place-link" type="url" id="link" placeholder="Ссылка на картинку" required/>
-            <span className="pop-up__input-error link-error">!!!</span>          
-          </label>
-        </form>
-      </PopupWithForm>
+        <PopupWithForm
+          name="avatar"
+          title="Обновить аватар"
+          isOpen={this.state.isEditAvatarPopupOpen}
+          onClose={this.closeAllPopups}
+          buttonText="Сохранить"
+          >
+          <form className="pop-up__form" name="user-pic" noValidate>
+            <label className="pop-up__field"> 
+              <input className="pop-up__input pop-up__input_field_avatar-link" type="url" id="avatar" placeholder="Ссылка на аватар" required/>
+              <span className="pop-up__input-error avatar-error">!!!</span>          
+            </label>
+          </form>
+        </PopupWithForm>
 
-      <PopupWithForm name="confirm" title="Вы уверены?" buttonText="Да">
-        <form className="pop-up__form" name="user-confirm" noValidate>
-        </form>
-      </PopupWithForm>
+        <PopupWithForm 
+          name="place"
+          title="Новое место"
+          isOpen={this.state.isAddPlacePopupOpen}
+          onClose={this.closeAllPopups}
+          buttonText="Сохранить"
+          >
+          <form className="pop-up__form" name="place-form" noValidate>
+            <label className="pop-up__field">
+              <input className="pop-up__input pop-up__input_field_place-name" type="text" id="name" placeholder="Название" required minLength="2" maxLength="30"/>
+              <span className="pop-up__input-error name-error">!!!</span>          
+            </label>
+            <label className="pop-up__field"> 
+              <input className="pop-up__input pop-up__input_field_place-link" type="url" id="link" placeholder="Ссылка на картинку" required/>
+              <span className="pop-up__input-error link-error">!!!</span>          
+            </label>
+          </form>
+        </PopupWithForm>
 
-      <ImagePopup card={this.state.selectedCard} onClose={this.closeAllPopups} isOpen={this.state.isImagePopupOpen}/>
+        <PopupWithForm name="confirm" title="Вы уверены?" buttonText="Да">
+          <form className="pop-up__form" name="user-confirm" noValidate>
+          </form>
+        </PopupWithForm>
+
+        <ImagePopup card={this.state.selectedCard} onClose={this.closeAllPopups} isOpen={this.state.isImagePopupOpen}/>
+      </CurrentUserContext.Provider>
     </>)
   }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import userpic from '../images/userpic.jpg';
-import mestoApi from '../utils/Api';
 import Card from './Card';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 export default class Main extends React.Component {
+  static contextType = CurrentUserContext;
   constructor(props) {
     super(props);
     this.state = {
@@ -14,41 +15,23 @@ export default class Main extends React.Component {
     }
   }
 
-  componentDidMount() {
-    Promise.all([mestoApi.getUser(), mestoApi.getCards()])
-      .then(([info, cards]) => {
-        this.setState({
-          userName: info['user-name'],
-          userDescription : info['user-profession'],
-          userAvatar: info['user-pic'],
-          cards: cards,
-        })
-      })
-      .catch(err => console.log(`Failed to load initial info : ${err}`));
-  }
-
   render() { 
     return (
     <main>
       <section className="profile">
         <button className="profile__img-button" type="button">
-          <img className="profile__avatar" src={this.state.userAvatar} alt="Аватар" />
+          <img className="profile__avatar" src={this.context[`user-pic`]} alt="Аватар" />
           <div className="profile__overlay" onClick={this.props.onEditAvatar}></div>
         </button>
         <div className="profile__info">
-          <h1 className="profile__name">{this.state.userName}</h1>
+          <h1 className="profile__name">{this.context[`user-name`]}</h1>
           <button className="profile__eddit-button" type="button" onClick={this.props.onEditProfile}></button>
-          <p className="profile__description">{this.state.userDescription}</p>
+          <p className="profile__description">{this.context[`user-profession`]}</p>
         </div>
         <button className="profile__add-button" type="button" onClick={this.props.onAddPlace}></button>
       </section>
 
       <section className="photo-grid">
-      {
-        this.state.cards.map((card, i) => (
-          <Card card={card} key={card._id} onCardClick={this.props.onCardClick} />
-        ))
-      }
       </section>
     </main>
   )}
